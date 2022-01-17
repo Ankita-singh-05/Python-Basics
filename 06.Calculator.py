0ffef09fbd994140fee3a4995032d6fc760a46a0
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-17 20:16:08 +0530

    Basic Calculator

---
+++ 06.Calculator.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+result = num1 + num2
+result2 = num1 * num2
+result3 = num1 / num2
+
+print(f"Addition of the number is: {result}" )
+print(f"Multiplication of the number is: {result2}" )
+print(f"Division of the number is: {result3}" )
