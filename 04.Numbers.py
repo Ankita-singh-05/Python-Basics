7811be49e203ffbf5f6f1b41634e38257207fe9a
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-17 17:56:25 +0530

    Numbers in Python

---
+++ 04.Numbers.py
@@ -0,0 +1,35 @@
+from cgi import print_directory
+from math import *
+
+# Basic Arithmetic
+print( 3 + 2 ) #  - , *, /, %
+print(3 * (4 + 5))
+
+my_num = 5
+print(my_num)
+# converting number in strings
+print(str(my_num) + "my favourite number")
+
+# TypeError -- Unsupported operand type 
+# Python do not concatinate strings with numbers
+#print(my_num + "my favourite number") 
+
+# MATHS FUNCTION IN PYTHON
+
+my_num = -5 
+# ABSOLUTE FUNCTION
+# To print the absolute number
+print(abs(my_num)) 
+
+# POWER FUNCTION
+print(pow(2, 3))
+
+# MAXIMUM -- to print the maximum of the given number
+print(max(4, 6))
+
+# MINIMUM  -- to print the minimum of the given number
+print(min(4, 6))
+
+# ROUND FUNCTION
+print(round(4.9)) # o/p - 5
+
