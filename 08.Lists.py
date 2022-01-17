4b2d16bd92532c5099d42fa89fb68d0b6688301e
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-17 20:36:59 +0530

    Lists in Python

---
+++ 08.Lists.py
@@ -0,0 +1,22 @@
+
+places = ["Pune", "Satara", "Nashik", "Nagpur"]
+
+# To print the whole list
+print(places)
+
+# To access any of the elements in the list
+print(places[0])
+
+# to access all the elements after the index 1
+print(places[1:])
+
+print(places[0:2])
+
+# to print the last element
+print(places[-1])
+
+# Modifying the list
+places[1] = "Sangli"
+print(places)
+
+
