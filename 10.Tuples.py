24a194067c2ebef477526396efb8458779f42825
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-17 21:11:07 +0530

    Tuples in python

--- 10.Tuples.py
+++ 10.Tuples.py
@@ -3,4 +3,13 @@
 # Means it cannot be changed or modified
 
 coordinates = (4, 5)
+
+#coordinates[1] = 10 # this will show an error because tuples are immutable
+
 print(coordinates[1])
+
+# Lists of tuples
+coordinates2 = [(4, 5), (6, 7), (8, 9)]
+print(coordinates2[1])
+
+# 1.24
