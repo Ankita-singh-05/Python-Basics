ef281c9e3041967f0024a3264da26cdef45d7109
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-18 18:03:52 +0530

    Functions in Python

---
+++ 11.functions.py
@@ -0,0 +1,25 @@
+# FUNCTIONS IS A LINE OF CODE THAT IS USED TO PERFORM SOME SPECIFIC TASK
+# Code inside the function is followed by indentation
+
+def say_hi(Name): 
+    print("Hello World")  # function definition
+    print("Namaste")
+    print("Hola")
+    print("Hello" + Name)
+
+# sayHi()  #Function call
+say_hi("Ankita")
+
+# Workflow of Function
+def sayHello(): 
+    print("Namaste")
+
+print("Top")
+sayHello()
+print("Bottom")
+
+def users(name, age):
+    print("Hello " + name + " , you are " + age)
+
+users("Ankita", "23")
+users("Anjali", "22")
