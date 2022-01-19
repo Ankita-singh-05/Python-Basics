35267f0cf99e2e60c25f3659bd9156e00675932b
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-19 14:02:23 +0530

    For loop in python

---
+++ 19.ForLoop.py
@@ -0,0 +1,18 @@
+# For loop allows us to loop different collections
+
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Geeta", "Shivani", "Kalyani"]
+for index in range(len(friends)):
+    print(friends[index])
+
+for name in range(0, 3):
+    print(name)
+
+for i in range(5):
+    if i == 0:
+        print("first iteration")
+    else:
+        print("Not first")
