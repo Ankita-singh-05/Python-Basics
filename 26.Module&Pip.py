4f754a78f73d180e991d406bd708d87987428180
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-20 20:00:19 +0530

    Modules And Pip

---
+++ 26.Module&Pip.py
@@ -0,0 +1,13 @@
+# modules
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+
+# PIP  -- PIP INSTALLS PACKAGES OR PIP INSTALLS PYTHON 
+# Alternatively -- PREFERRED INSTALLER PROGRAM
+# It refers to a package manager
+# It is used to install the modules in python
+# It is used to manage update, uninstall, install modules
+
+# Installing Module -- go to command prompt
+# eg- pip install python-docx
