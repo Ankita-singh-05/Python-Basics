a6fe897ba2003f29602fd531781756807c82ed5f
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-20 23:45:23 +0530

    Classes And Object

---
+++ 27.Classes&Objects.py
@@ -0,0 +1,8 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 4.3, True)
+
+print(student1.name)
+print(student2.name)
+print(student1.is_on_probation)
