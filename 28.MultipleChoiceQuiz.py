ce974a5db8f99fc96764f295e74fba39c3bd7863
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-21 23:19:35 +0530

    Multiple choice quiz

--- 28.MultipleChoiceQuiz.py
+++ 28.MultipleChoiceQuiz.py
@@ -1,12 +1,9 @@
 from Question import Question
 
-
-import Question from Question
-
 question_prompts = [
    "What color are apples? \n(a) Red/Green \n(b) Purple \n(c) Orange\n\n",
    "What color are bananas? \n(a) Teal \n(b) Magenta \n(c) Yellow \n\n",
-   "What color are straberries? \n(a) Yellow \n(b) Red \n(c) Blue \n\n"
+   "What color are strawberries? \n(a) Yellow \n(b) Red \n(c) Blue \n\n"
 ]
 
 question = [
@@ -14,3 +11,13 @@ question = [
     Question(question_prompts[1], "c"),
     Question(question_prompts[2], "b")
 ]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(question)
